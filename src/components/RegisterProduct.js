c4642953898ed0de058c2bd6d5580d8a5b1b76bd
Axel0944 <akahasamnis@gmail.com>
2022-03-11 02:03:23 -0800

    Product Registration

--- src/components/RegisterProduct.js
+++ src/components/RegisterProduct.js
@@ -7,7 +7,9 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import Input from '@material-ui/core/Input';
-import EasyRentalAppBar from "./EasyRentalAppBar";
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 export default function RegisterProduct(){
@@ -18,27 +20,35 @@ export default function RegisterProduct(){
     const[price, setPrice] = useState('');
     const[image, setImage] = useState('');
     const[description, setDescription] = useState('');
+    const[availability, setAvailability] = useState(true);
+    const[specififcation, setSpecification] = useState('');
+    const[tags, setTags] = useState('');
     
     const onImageChange = (e) =>{
         setImage([...e.target.files])
     }
 
+    const handleChange = () =>{
+        setAvailability(current => !current);
+    }
+
     return (
-        <div>
-            <EasyRentalAppBar/>
-            <Container>
+        <Container>
             <Paper elevation={2} className='login-paper' >
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Register
+                    Register Product
                 </Typography>
                 <form className='login-form' noValidate autoComplete='off'>
                     <TextField label='Product Name'  variant='outlined' fullWidth value={name}
                     onChange={(e)=>setName(e.target.value)} className="small-margin-below"/>
                     <TextField label='Price'  variant='outlined' fullWidth value={price}
                     onChange={(e)=>setPrice(e.target.value)} className="small-margin-below"/>
+                    <TextField label='Relevant tags'  variant='outlined' fullWidth value={tags}
+                    onChange={(e)=>setTags(e.target.value)} className="small-margin-below"/>
                     <TextField label='Description'  variant='outlined' fullWidth value={description}
                     onChange={(e)=>setDescription(e.target.value)} multiline className="small-margin-below"/>
-                    <br></br>
+                    <TextField label='Specifications'  variant='outlined' fullWidth value={specififcation}
+                    onChange={(e)=>setSpecification(e.target.value)} multiline className="small-margin-below"/>
                     <label>Picture: </label>
                     <input type="file" className="small-margin-below" onChange={onImageChange} />  
                     <br></br>   
@@ -47,7 +57,6 @@ export default function RegisterProduct(){
                 </form>
             </Paper>
         </Container>
-        </div>
     )
 
 }
